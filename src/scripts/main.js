49112300561426f2a76062d6386b6a8ffdc5e962
Sofiia <sofiiamuz@gmail.com>
2025-12-29 00:45:58 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,8 @@
 'use strict';
 
 // Uncomment the next lines to use your game instance in the browser
-const Game = require('../modules/Game.class');
+import Game from '../modules/Game.class';
+
 const game = new Game();
 
 const allElements = document.querySelectorAll('.field-cell');
